803770d3903b2c069b2b3f3eedeb4b59cfe2c5a2
rdeshu <40371856+rdeshu@users.noreply.github.com>
2025-01-06 19:26:04 -0600

    Update params_base.py

--- params_base.py
+++ params_base.py
@@ -1,1 +1,27 @@
+# parameters/params_base.py
 
+from abc import ABC, abstractmethod
+
+
+class ParametersBase(ABC):
+    """
+    Abstract base class for parameters.
+    Defines methods to set and get parameter values.
+    """
+
+    @abstractmethod
+    def set_params(self, **kwargs):
+        """
+        Abstract method to set parameters dynamically.
+        :param kwargs: Dictionary of parameter values.
+        """
+        pass
+
+    @abstractmethod
+    def get_param(self, key: str):
+        """
+        Abstract method to get a parameter by key.
+        :param key: Name of the parameter.
+        :return: Value of the parameter.
+        """
+        pass
